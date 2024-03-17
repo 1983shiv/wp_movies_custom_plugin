f58539bbac6a46067b80d779cb5ab03540abf20b
= <=>
2024-03-17 09:58:34 +0530

    added basic skeleton for building this custom plugin

---
+++ includes/class-movies-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://github.com/1983shiv
+ * @since      1.0.0
+ *
+ * @package    Movies
+ * @subpackage Movies/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Movies
+ * @subpackage Movies/includes
+ * @author     Shiv Srivastava <ninjatech.app@gmail.com>
+ */
+class Movies_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-movies-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://github.com/1983shiv
+ * @since      1.0.0
+ *
+ * @package    Movies
+ * @subpackage Movies/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Movies
+ * @subpackage Movies/includes
+ * @author     Shiv Srivastava <ninjatech.app@gmail.com>
+ */
+class Movies_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
